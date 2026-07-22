1060176739e051b62fa08cff42eaa9e4c47a6081
ARYA RAMDASI <arya.ramdasi@cumminscollege.in>
2026-07-22 08:21:00 +0530

    Add: lru-cache - submission-0

---
+++ Data Structures & Algorithms/lru-cache/submission-0.java
@@ -0,0 +1,58 @@
+class LRUCache {
+
+    private ArrayList <int[]> cache;
+    private int capacity;
+
+    public LRUCache(int capacity) {
+        this.capacity = capacity;
+        this.cache = new ArrayList<>();    
+    }
+    
+    public int get(int key) {
+        for(int i = 0; i <cache.size(); i++){
+            if(cache.get(i)[0] == key){
+                int[] temp = cache.remove(i);
+                cache.add(temp);
+                return temp[1];
+            }
+        }
+        return -1;
+    }
+    
+    public void put(int key, int value) {
+        for(int i = 0; i < cache.size(); i++){
+            if(cache.get(i)[0] == key){
+                int[] temp = cache.remove(i);
+                temp[1] = value;
+                cache.add(temp);
+                return;
+            }
+        }
+         
+        if(capacity == cache.size()){
+           cache.remove(0);
+        }
+        cache.add(new int[]{key, value});
+        
+    }
+
+    /*
+     public LRUCache(int capacity) {
+        this.capacity = capacity;
+        this.cache = cache;    
+    }
+    
+    public int get(int key) {
+        for(int i = 0; i <cache.size; i++){
+            if(cache.get(i)[0] == key){
+                int temp = cache.remove(i);
+                cache.add(temp);
+                return temp[1];
+            }
+        }
+    }
+    
+    public void put(int key, int value) {
+        
+    }*/
+}
