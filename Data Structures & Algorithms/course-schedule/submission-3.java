dcdb0082d363a75542baf86ab108d50bfe8eb99a
ARYA RAMDASI <arya.ramdasi@cumminscollege.in>
2026-08-22 13:25:04 +0530

    Add: course-schedule - submission-3

---
+++ Data Structures & Algorithms/course-schedule/submission-3.java
@@ -0,0 +1,38 @@
+class Solution {
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+     //topo sort using Kahn's algo"
+    int[] inDegree = new int[numCourses];
+    List<List<Integer>> adj = new ArrayList<>();
+    //for each course w indegree 0 in numCourse
+    Queue<Integer> q = new LinkedList<>();
+
+    for(int i = 0; i < numCourses; i++){
+        adj.add(new ArrayList<>());
+    }
+    for(int[] pre : prerequisites){//0th element- course
+    //1st element- prerequisite
+        inDegree[pre[1]]++;
+        adj.get(pre[0]).add(pre[1]);
+    }
+    for(int i = 0; i < numCourses; i++){
+        if(inDegree[i] == 0){
+            q.add(i);
+        }
+    }
+    int finish = 0;
+    while(!q.isEmpty()){
+        int node = q.poll();//remove from front
+        finish++;
+        for(int n : adj.get(node)){
+            //get list for that node
+            inDegree[n]--;
+            if(inDegree[n] == 0){
+                q.add(n);
+            }
+        }
+    }
+    return finish == numCourses;
+
+
+    }
+}
